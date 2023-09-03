1433ab9ee977cc145b3d171615e7fda5be91f97f
Zablon Dawit <zablondawit@pelmorex.com>
2023-09-03 15:10:03 +0300

    feat: react transition group

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,49 @@
+import { CSSProperties, useRef, useState } from "react";
 import "./App.css";
 import { square, container } from "./application.css";
+import { Transition, TransitionStatus } from "react-transition-group";
 
-const Square = () => <div className={square} />
+const duration = 300;
+const defaultStyle: CSSProperties = {
+  transition: `translate ${duration}ms ease-in-out`,
+  translate: "0 50px",
+  opacity: "1"
+};
+
+const transitionStyles: Record<TransitionStatus, CSSProperties> = {
+  entering: { opacity: 1, translate: "0 0" },
+  entered: { opacity: 1, translate: "0 0" },
+  exiting: { opacity: 0, translate: "0 50px"},
+  exited: { opacity: 0, translate: "0 50px" },
+  unmounted: {},
+};
+
+const Square: React.FC<{ inProp: boolean }> = ({ inProp }) => {
+  const nodeRef = useRef(null);
+
+  return (
+    <Transition nodeRef={nodeRef} in={inProp} timeout={duration}>
+      {(state) => (
+        <div
+          className={square}
+          ref={nodeRef}
+          style={{
+            ...defaultStyle,
+            ...transitionStyles[state],
+          }}
+        />
+      )}
+    </Transition>
+  );
+};
 
 function App() {
+  const [status, setStatus] = useState(true);
+
   return (
     <div className={container}>
-      <Square />
-      <button>Click to Hide/Show</button>
+      <Square inProp={status} />
+      <button onClick={() => setStatus(!status)}>Click to Hide/Show</button>
     </div>
   );
 }
