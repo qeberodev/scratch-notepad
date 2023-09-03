9bde8f7264adfab97c1b66aa9ea8c8f6c41ada95
Zablon Dawit <zablondawit@pelmorex.com>
2023-09-03 18:58:39 +0300

    feat: understand react spring api

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,60 @@
-import React from "react";
-import "./App.css";
-import { container, square } from "./application.css";
+import "./App.css"
+import React, {
+    MutableRefObject,
+    useCallback,
+    useMemo,
+    useRef,
+    useState,
+} from "react"
+import { container, square } from "./application.css"
+import { animated, useSpring } from "@react-spring/web"
 
-const Square: React.FC = () => <div className={square} />;
+type Animatable = {
+    animate: () => void
+}
+const Square: React.FC<{
+    api: MutableRefObject<Animatable | undefined>
+}> = ({ api: apiRef }) => {
+    const [state, setAnimationState] = useState(false)
 
-function App() {
-  return (
-    <div className={container}>
-      <Square />
-      <button>Click to Hide/Show</button>
-    </div>
-  );
+    const [springs, api] = useSpring(() => ({
+        from: { x: 0 },
+    }))
+    const animatableApi = useMemo((): Animatable => {
+        return {
+            animate: () => {
+                api.start({
+                    from: {
+                        x: 0,
+                    },
+                    to: {
+                        x: 50,
+                    },
+                    reverse: state,
+                })
+                setAnimationState(!state)
+            },
+        }
+    }, [api, state])
+
+    apiRef.current = animatableApi
+
+    return <animated.div className={square} style={springs} />
 }
 
-export default App;
+function App() {
+    const apiRef = useRef<Animatable>()
+
+    const handlePlay = useCallback(() => {
+        apiRef.current?.animate()
+    }, [apiRef])
+
+    return (
+        <div className={container}>
+            <Square api={apiRef} />
+            <button onClick={handlePlay}>Click to Hide/Show</button>
+        </div>
+    )
+}
+
+export default App
