a4b00f2c038a49cab620611000b986e4ef36ab80
Zablon Dawit <zabjd22@gmail.com>
2023-10-06 01:53:41 +0300

    feat: add more path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
-import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
+import { defineConfig } from "vite"
+import { resolve } from "path"
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin"
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), vanillaExtractPlugin()],
+    resolve: {
+        alias: {
+            "@app": resolve(__dirname, "src"),
+            "@model": resolve(__dirname, "src", "model"),
+            "@components": resolve(__dirname, "src", "components"),
+            "@hooks": resolve(__dirname, "src", "hooks"),
+        },
+    },
 })
